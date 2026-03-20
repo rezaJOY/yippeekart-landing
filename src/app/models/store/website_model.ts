6ce0b322c136c8a1c953286b72f4b0c47f521e8e
Reza <52202888+rezaJOY@users.noreply.github.com>
2026-03-20 23:29:03 +0600

    Add files via upload

---
+++ src/app/models/store/website_model.ts
@@ -0,0 +1,11 @@
+export type WebsiteSchema = {
+  aboutUs: string;
+  privacyAndPolicy: string;
+  returnAndRefund: string;
+  termsAndCondition: string;
+  bannerDescription: string;
+  bannerTitle: string;
+  description: string;
+  subTitle: string;
+  title: string;
+};
