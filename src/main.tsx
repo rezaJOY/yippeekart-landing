ba8ef8cc8de0f6bb08494b44faf43f3c4e2c5213
Reza <52202888+rezaJOY@users.noreply.github.com>
2026-03-20 23:38:40 +0600

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import { App } from "@/app";
+import "@/shared/styles/common.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.getElementById("app")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
