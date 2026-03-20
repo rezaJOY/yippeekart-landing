07c9052b8cca7b601266d7aabf3a9bbabc8736b5
Reza <52202888+rezaJOY@users.noreply.github.com>
2026-03-20 23:26:36 +0600

    Add files via upload

---
+++ src/shared/ui/layout/Layout.tsx
@@ -0,0 +1,13 @@
+import { Footer, Header } from "@/shared/ui";
+import { Outlet } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+export default Layout;
